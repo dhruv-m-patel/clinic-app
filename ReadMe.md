23a96e4f2f1f969015f10fad1e76f51234b02e6b
Dhruv Patel <dpatel@gasbuddy.com>
2018-10-10 17:26:05 -0700

    Use clinic doctor correctly

--- ReadMe.md
+++ ReadMe.md
@@ -4,3 +4,6 @@ Run this commands for workshop:
 docker pull mafintosh/npm-in-mongo
 docker run -it -p 27017:27017 -d mafintosh/npm-in-mongo
 ```
+
+
+Grab code from here: https://gist.github.com/mcollina/0bc3c9e99fc33445785206c4f46a4d7e
--- startServer.js
+++ startServer.js
@@ -1,9 +1,25 @@
-const http = require('http');
+const mongojs = require('mongojs')
+const fastify = require('fastify')
+const computeMagic = require('@nearform/compute-magic')
 
-function createServer() {
-  http.createServer(function (req, res) {
-    res.end('Hello World!\n');
-  }).listen(3000);
-}
+const db = mongojs('localhost:27017/npm')
+const c = db.collection('modules')
+const app = fastify()
 
-createServer();
+// get 5 newest and 5 oldest, no index
+app.get('/', function (req, reply) {
+  c.find().sort({modified: -1}).limit(5, function (err, newest) {
+    if (err) return reply.code(500).send(err)
+    c.find().sort({modified: 1}).limit(5, function (err, oldest) {
+      if (err) return reply.code(500).send(err)
+      const magic = computeMagic(newest, oldest)
+      reply.send({
+        magic,
+        newest,
+        oldest
+      })
+    })
+  })
+})
+
+app.listen(3000)
